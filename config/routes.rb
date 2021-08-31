487f7ecba384d5d26ed7b89a24c24673b3527166
Noam Lev <levno.ro@gmail.com>
2021-08-31 15:04:56 +0200

    Fixed a typo in models

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :stocks do
-    resources :acquistions, only: [:new, :create, :update]
+    resources :acquisitions, only: [:new, :create, :update]
   end
   resources :followed_stocks, except: [:update, :edit]
   get "dashboard", to: "pages#dashboard"
