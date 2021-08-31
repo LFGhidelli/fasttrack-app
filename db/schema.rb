58641696dfb58a59d3d37d4c92e1b5b704df28e3
Jean-Sebastien Morel <js.morel130@gmail.com>
2021-08-31 12:43:04 +0200

    creation of routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_161709) do
+ActiveRecord::Schema.define(version: 2021_08_30_162516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "acquisitions", force: :cascade do |t|
+    t.float "value_bought"
+    t.float "amount_bought"
+    t.date "date_bought"
+    t.bigint "user_id", null: false
+    t.bigint "stock_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["stock_id"], name: "index_acquisitions_on_stock_id"
+    t.index ["user_id"], name: "index_acquisitions_on_user_id"
+  end
+
+  create_table "followed_stocks", force: :cascade do |t|
+    t.string "name"
+    t.bigint "stock_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["stock_id"], name: "index_followed_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_followed_stocks_on_user_id"
+  end
+
+  create_table "stock_day_infos", force: :cascade do |t|
+    t.float "closing_price"
+    t.date "closing_date"
+    t.bigint "stock_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["stock_id"], name: "index_stock_day_infos_on_stock_id"
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +65,9 @@ ActiveRecord::Schema.define(version: 2021_08_30_161709) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "acquisitions", "stocks"
+  add_foreign_key "acquisitions", "users"
+  add_foreign_key "followed_stocks", "stocks"
+  add_foreign_key "followed_stocks", "users"
+  add_foreign_key "stock_day_infos", "stocks"
 end
