5144b3ee313007167835c7079ff545fef42746d3
Noam Lev <levno.ro@gmail.com>
2021-09-01 15:33:19 +0200

    portfolio data calculations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,11 +14,14 @@ class PagesController < ApplicationController
     @grouped_acquisitions = @acquisitions.group_by(&:stock_id)
     @stock_total_value = {}
     @grouped_acquisitions.each do |stock_id, acquisition_array|
-      total = 0
+      total_money_value = 0
+      total_stock_count = 0
       acquisition_array.each do |acquisition|
-        total += acquisition.value_bought * acquisition.amount_bought
+        total_money_value += acquisition.value_bought * acquisition.amount_bought
+        total_stock_count += acquisition.amount_bought
       end
-      @stock_total_value[stock_id] = { total: total }
+      @stock_total_value[stock_id] = {
+        total_money_value: total_money_value, total_stock_count: total_stock_count }
     end
   end
 end
