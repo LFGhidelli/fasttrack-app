967adf6d395af0e8c68fdaa1d260f408d58f8646
victor della faille <victordellafaille@gmail.com>
2021-09-02 14:45:47 +0200

    done the backend

--- app/controllers/followed_stocks_controller.rb
+++ app/controllers/followed_stocks_controller.rb
@@ -3,16 +3,17 @@ class FollowedStocksController < ApplicationController
     @followedstocks = current_user.followed_stocks
     @followedstock = FollowedStock.new
     @stocks = Stock.all
+    followed_acquisition_portfolio
   end
 
   def create
-    @followedstock = FollowedStock.new(followedstock_params)
+    @followedstock = FollowedStock.new(followed_stock_params)
     @followedstock.user = current_user
     @followedstock.save
     if @followedstock.save
       redirect_to followed_stocks_path(@followedstock)
     else
-      render :new
+      redirect_to followed_stocks_path(@followedstock), notice: 'this stock is already in your list :('
     end
   end
 
@@ -26,9 +27,21 @@ class FollowedStocksController < ApplicationController
     redirect_to followed_stocks_path
   end
 
-  private
-
-  def followedstock_params
-    params.require(:followed_stock).permit(:stock_id)
+  def followed_acquisition_portfolio
+    set_client
+    @followed_stocks = current_user.followed_stocks
+    @grouped_followed_stocks = @followed_stocks.group_by(&:stock_id)
+    @stock_market_prices = {}
+    @grouped_followed_stocks.each do |stock_id, followed_stock_array|
+      stock_api = @client.quote(Stock.find(stock_id).ticker)
+      @stock_market_prices[stock_id] = {
+        latest_price: stock_api.latest_price.round(1), change_percent_s: stock_api.change_percent_s }
+    end
   end
+
+    private
+
+    def followed_stock_params
+      params.require(:followed_stock).permit(:stock_id)
+    end
 end
